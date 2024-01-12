2b927031d1e00cc7d9c25695ca88898541a8247d
Kevin Pham <keevan.pham@gmail.com>
2024-01-12 16:48:46 +1100

    chore: add ci

--- version.php
+++ version.php
@@ -26,6 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2021060200;
 $plugin->requires = 2016120500; // Moodle 3.2.
+$plugin->supported = [310, 403];
 $plugin->component = 'availability_mobileapp';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = '4.0';
